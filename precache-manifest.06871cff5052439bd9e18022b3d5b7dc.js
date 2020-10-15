68ab481edc44a55569750fe0b8ef7a028d076d7f
all-github <dngnta@daum.net>
2020-10-15 16:27:45 +0900

    Updates

---
+++ precache-manifest.06871cff5052439bd9e18022b3d5b7dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd5f6577a419bcbd18da442180ddc1f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "adaa1063ef5fcdecf109",
+    "url": "/todolist/static/css/main.ea0fccf0.chunk.css"
+  },
+  {
+    "revision": "70d24e14d0e51be8050e",
+    "url": "/todolist/static/js/2.3d1b521d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todolist/static/js/2.3d1b521d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaa1063ef5fcdecf109",
+    "url": "/todolist/static/js/main.f948e999.chunk.js"
+  },
+  {
+    "revision": "be1ea64cf07e1e2b9473",
+    "url": "/todolist/static/js/runtime-main.992438bd.js"
+  }
+]);
